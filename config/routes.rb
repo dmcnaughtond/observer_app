aed18b3cd9bdc804a27a452c35bc72a28bac2e16
Dan <d.duncan.mcn@gmail.com>
2018-07-11 12:33:02 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  get '/static_pages/home', :to => 'static_pages#home'
-  get '/static_pages/help', :to => 'static_pages#help'
+	root 'static_pages#home'
+  	get '/static_pages/home', :to => 'static_pages#home'
+  	get '/static_pages/help', :to => 'static_pages#help'
 
   # root                'static_pages#home'
   # get 'help'      =>  'static_pages#help'
