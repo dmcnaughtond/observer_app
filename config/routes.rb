99376f0c92af2873c3bf787464ae719fe63f6bed
Dan <d.duncan.mcn@gmail.com>
2018-07-16 18:12:00 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
 	root 'static_pages#home'
     get '/signup', to: 'users#new'
+    post '/signup',  to: 'users#create'
+    
+    resources :users
   	
 
   # root                'static_pages#home'
