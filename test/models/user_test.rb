a30951277c3eb0c1e10567f9c9affec217d3ef5c
Dan <d.duncan.mcn@gmail.com>
2018-07-25 08:38:42 -0500

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,9 +77,14 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-    test "password should have a minimum length" do
+  test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 7
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
 
 end
